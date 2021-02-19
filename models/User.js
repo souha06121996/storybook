41014b515370ddcb261d60a44393638cabaa7baf
souheib.abdelhak <souheib.abdelhak@ens.uvsq.fr>
2021-02-20 00:12:54 +0100

    started UI

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema =mongoose.Schema ;
+
+//Create Shema 
+const UserSchema =new Schema(
+{
+    googleID:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    firstName:{
+        type:String,
+        
+    },
+    lastName:{
+        type:String,
+        
+    },
+    image:{
+        type:String,
+        
+    }
+    
+}
+
+);
+
+//Create collection and schema
+mongoose.model('users',UserSchema)
